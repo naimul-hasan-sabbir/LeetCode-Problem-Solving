7f06e08a58681b81f9f5ceff0961bf780951adb5
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-01-29 11:01:13 +0600

    Day 6

---
+++ src/com/NaimulHasanSabbir/valid_parentheses.java
@@ -0,0 +1,25 @@
+package com.NaimulHasanSabbir;
+
+import java.util.Stack;
+
+public class valid_parentheses {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.peek();
+                if((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
