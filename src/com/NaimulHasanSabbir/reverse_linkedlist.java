00e26f0a6fad6d2fa17de8c741e35a9e66aeeb2f
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-02-05 18:27:53 +0600

    Day 12

---
+++ src/com/NaimulHasanSabbir/reverse_linkedlist.java
@@ -0,0 +1,28 @@
+package com.NaimulHasanSabbir;
+
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
+class reverse_linkedlist {
+    public ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode current = head;
+
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
