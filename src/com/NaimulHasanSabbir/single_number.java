1e1f7bde079da381b3c380af395e3d624b74edef
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-02-29 15:51:18 +0600

    Day 34

---
+++ src/com/NaimulHasanSabbir/single_number.java
@@ -0,0 +1,11 @@
+package com.NaimulHasanSabbir;
+
+public class single_number {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i =0;i < nums.length; i++){
+            result = result^nums[i];
+        }
+        return result;
+    }
+}
