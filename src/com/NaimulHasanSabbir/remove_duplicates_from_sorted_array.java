b17cbb8ac40e8996a4c5c3058b32f440dec7ca9d
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-03-25 13:54:14 +0600

    Day 57 update

--- src/com/NaimulHasanSabbir/remove_duplicates_from_sorted_array.java
+++ src/com/NaimulHasanSabbir/remove_duplicates_from_sorted_array.java
@@ -5,7 +5,7 @@ public class remove_duplicates_from_sorted_array {
         int res = 1;
         for(int i = 1; i < nums.length; i++){
             if(nums[i] != nums[i - 1]){
-                nums[j] = nums[i];
+                nums[res] = nums[i];
                 res++;
             }
         }
