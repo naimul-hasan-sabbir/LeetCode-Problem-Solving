ad93c0c7dbdbbc4a92cb26e1de2dad120ee96856
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-11-11 16:41:52 +0600

    Linked List and Arrays Day - 4

--- src/com/NaimulHasanSabbir/LinkedListAndArrays/problem3/Better_Solution/ThreeSum.java
+++ src/com/NaimulHasanSabbir/LinkedListAndArrays/problem3/Better_Solution/ThreeSum.java
@@ -1,5 +1,8 @@
 package com.NaimulHasanSabbir.LinkedListAndArrays.problem3.Better_Solution;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -11,8 +14,28 @@ public class ThreeSum {
             HashSet<Integer> hashSet = new HashSet<>();
             for (int j = i+1; j < arr.length; j++){
                 int third = -(arr[i] + arr[j]);
+
+                if(hashSet.contains(third)){
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                hashSet.add(arr[j]);
             }
         }
-
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> ans = triplet(arr);
+        for(List<Integer> list : ans){
+            System.out.print("[");
+            for(int it: list){
+                System.out.print(it+" ");
+            }
+            System.out.print("]");
+        }
+        System.out.println();
     }
 }
