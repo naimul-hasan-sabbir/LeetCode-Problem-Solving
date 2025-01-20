e457c88bf3b7f3eccd966acfdbe3975ad5912f8e
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2025-01-20 11:36:23 +0600

    stack medium

--- src/com/NaimulHasanSabbir/Stack/CarFleet.java
+++ src/com/NaimulHasanSabbir/Stack/CarFleet.java
@@ -1,5 +1,28 @@
 package com.NaimulHasanSabbir.Stack;
 
-public class CarFleet {
+import java.util.Arrays;
+import java.util.Stack;
 
+public class CarFleet {
+    public static int carFleet(int target, int[]position, int[]speed){
+        int[][] pair = new int[position.length][2];
+        for(int i = 0; i < position.length; i++){
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+        Arrays.sort(pair, (a,b) -> Integer.compare(b[0], a[0]));
+        Stack<Double> stack = new Stack<>();
+        for(int[] p : pair ){
+            stack.push((double) (target - p[0]) / p[1]);
+            if (stack.size() >= 2 && stack.peek() <= stack.get(stack.size()-2)){
+                stack.pop();
+            }
+        }
+        return stack.size();
+    }
+    public static void main(String[] args) {
+        int[] pos = {10,8,0,5,3};
+        int[] speed = {2,4,1,1,3};
+        System.out.println(carFleet(12, pos, speed));
+    }
 }
---
+++ src/com/NaimulHasanSabbir/Stack/SimlifyPath.java
@@ -0,0 +1,27 @@
+package com.NaimulHasanSabbir.Stack;
+
+import java.util.Stack;
+
+public class SimlifyPath {
+    public String simlifyPath(String path) {
+        Stack<String> stack = new Stack<String>();
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : (path + "/").toCharArray()){
+            if(c == '/'){
+                if (sb.toString().equals("..")){
+                    if (!stack.isEmpty()){
+                        stack.pop();
+                    }
+                }
+                else if (!sb.toString().equals("") && !sb.toString().equals(".")){
+                    stack.push(sb.toString());
+                }
+                sb.setLength(0);
+            }else{
+                sb.append(c);
+            }
+        }
+        return "/" + String.join("/", stack);
+    }
+}
