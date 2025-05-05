c1cc20dd1e376bd8b16228144c0bb718e2d6b6c5
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2025-05-05 15:34:11 +0600

    LeetCode 2486

---
+++ src/com/NaimulHasanSabbir/String/Append_Character_to_String_to_Make_Subsequence/Tester.java
@@ -0,0 +1,12 @@
+package com.NaimulHasanSabbir.String.Append_Character_to_String_to_Make_Subsequence;
+
+public class Tester {
+    public static void main(String[] args) {
+        AppendCharactersToStringToMakeSubsequence test = new AppendCharactersToStringToMakeSubsequence();
+        String s = "coaching";
+        String t = "coding";
+
+        System.out.println(test.appendCharacters(s, t));
+
+    }
+}
